84a94087bc2a13515990a1c55c7edaeb87456684
Alex <glassprominsk@gmail.com>
2016-12-15 23:20:08 +0300

    Первоначальный коммит

---
+++ helloWorld.js
@@ -0,0 +1,24 @@
+var http = require('http');
+
+http.createServer(function(req,res){
+  // Приводим URL к единому виду путем удаления
+  // строки запроса, необязательной косой черты
+  // в конце строки и приведения к нижнему регистру
+  var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+  switch(path) {
+    case '':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('Homepage');
+            break;
+    case '/about':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('0');
+            break;
+    default:
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.end('Not search');
+            break;
+  }
+}).listen(3000);
+
+console.log('Сервер запущен на localhost:3000; нажмите Ctrl-С для завершения....');
